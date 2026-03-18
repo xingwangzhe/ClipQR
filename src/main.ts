be9b4bd1eeac2a2f8b919f6e247f1789a61c5c70
王兴家 <xingwangzhe@outlook.com>
2026-03-18 19:35:34 +0800

    fmt && add tray

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,28 @@
 import { createApp } from 'vue'
+import { Menu } from '@tauri-apps/api/menu';
 import './style.css'
 import App from './App.vue'
-
+import { TrayIcon } from '@tauri-apps/api/tray';
+import { platform } from '@tauri-apps/plugin-os';
 createApp(App).mount('#app')
+const menu = await Menu.new({
+  items: [
+    {
+      id: 'quit',
+      text: '完全退出',
+    },
+  ],
+});
+
+const currentPlatform = platform();
+
+let icon = '../src-tauri/icons/icon.png'
+if (currentPlatform === 'windows') {
+  icon = '../src-tauri/icons/icon.ico';
+}
+
+const tray = await TrayIcon.new({
+  icon: icon,
+  menu,
+  menuOnLeftClick: true,
+});
