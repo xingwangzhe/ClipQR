6dc52c20fbb2796ad78c2c5f27fe625eda839b6f
王兴家 <xingwangzhe@outlook.com>
2026-03-19 17:17:33 +0800

    vite:配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   build: {
     minify: 'oxc',
     cssMinify: true
